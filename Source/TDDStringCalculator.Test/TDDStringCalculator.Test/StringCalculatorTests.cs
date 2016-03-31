ebf0abdc1cbb547497661d20423f343a770e1287
Dragan Stepanovic <dragan.stepanovic@devtechgroup.com>
2016-03-31 16:55:38 +0200

    builder pattern added

--- Source/TDDStringCalculator.Test/TDDStringCalculator.Test/StringCalculatorTests.cs
+++ Source/TDDStringCalculator.Test/TDDStringCalculator.Test/StringCalculatorTests.cs
@@ -9,118 +9,146 @@ using Xunit;
 
 namespace TDDStringCalculator.Test
 {
-    public class StringCalculatorTests
-    {
-        StringCalculator _stringCalc = new StringCalculator();
-        [Fact]
-        public void Add_ForEmptyStringInput_ReturnsZero()
-        {
-            //ARRANGE
-            int expected = 0;
-            string emptyString = "";
-            
-            //ACT
-            int actual = _stringCalc.Add(emptyString);
+	public class StringCalculatorTests
+	{
+		private StringCalculator _stringCalc;
 
-            //ASSERT
-            Assert.Equal(expected, actual);
-        }
+		public StringCalculatorTests()
+		{
+			_stringCalc = new StringCalcBuilder().Build();
+		}
 
-        [Fact]
-        public void Add_ForOneNumberAsStringInput_ReturnThatNumber()
-        {
-            // ARRANGE
-            int expected = 5;
-            string oneNumberAsString = "5";
+		[Fact]
+		public void Add_ForEmptyStringInput_ReturnsZero()
+		{
+			//ARRANGE
+			int expected = 0;
+			string emptyString = "";
+			
+			//ACT
+			int actual = _stringCalc.Add(emptyString);
 
-            // ACT 
-            int actual = _stringCalc.Add(oneNumberAsString);
+			//ASSERT
+			Assert.Equal(expected, actual);
+		}
 
-            // ASSERT
-            Assert.Equal(expected, actual);
-        }
+		[Fact]
+		public void Add_ForOneNumberAsStringInput_ReturnThatNumber()
+		{
+			// ARRANGE
+			int expected = 5;
+			string oneNumberAsString = "5";
 
-        [Fact]
-        public void Add_ForCommaSeparatedTwoNumbersAsStringInput_ReturnsSumOfThoseNumbers()
-        {
-            // ARRANGE
-            int expected = 10;
-            string twoNumbersAsString = "3,7";
-            
-            // ACT
-            int actual = _stringCalc.Add(twoNumbersAsString);
+			// ACT 
+			int actual = _stringCalc.Add(oneNumberAsString);
 
-            // ASSERT
-            Assert.Equal(expected, actual);
-        }
+			// ASSERT
+			Assert.Equal(expected, actual);
+		}
 
-        [Fact]
-        public void Add_ForAnyAmountOfNumbersAsStringInput_ReturnsTheirSum()
-        {
-            // ARRANGE
-            int expected = 6;
-            string threeNumbers = "1,2,3";
+		[Fact]
+		public void Add_ForCommaSeparatedTwoNumbersAsStringInput_ReturnsSumOfThoseNumbers()
+		{
+			// ARRANGE
+			int expected = 10;
+			string twoNumbersAsString = "3,7";
+			
+			// ACT
+			int actual = _stringCalc.Add(twoNumbersAsString);
 
-            // ACT
-            int actual = _stringCalc.Add(threeNumbers);
+			// ASSERT
+			Assert.Equal(expected, actual);
+		}
 
-            // ASSERT
-            Assert.Equal(expected, actual);
-        }
+		[Fact]
+		public void Add_ForAnyAmountOfNumbersAsStringInput_ReturnsTheirSum()
+		{
+			// ARRANGE
+			int expected = 6;
+			string threeNumbers = "1,2,3";
 
-        [Fact]
-        public void Add_ForCommaAndNewLineSeparatedNumbersAsStringInput_ReturnTheirSum()
-        {
-            // ARRANGE
-            int expected = 6;
-            string newLineInsteadOfCommaString = "1\n2,3";
+			// ACT
+			int actual = _stringCalc.Add(threeNumbers);
 
-            // ACT
-            int actual = _stringCalc.Add(newLineInsteadOfCommaString);
+			// ASSERT
+			Assert.Equal(expected, actual);
+		}
 
-            // ASSERT
-            Assert.Equal(expected, actual);
-        }
+		[Fact]
+		public void Add_ForCommaAndNewLineSeparatedNumbersAsStringInput_ReturnTheirSum()
+		{
+			// ARRANGE
+			int expected = 6;
+			string newLineInsteadOfCommaString = "1\n2,3";
 
-        [Fact]
-        public void Add_ProvidingDelimiterWithNumersAsStringInput_ReturnTheirSum()
-        {
-            // ARRANGE
-            int expected = 3;
-            string numbersWithDelimiter = "//;\n1;2";
+			// ACT
+			int actual = _stringCalc.Add(newLineInsteadOfCommaString);
 
-            // ACT
-            int actual = _stringCalc.Add(numbersWithDelimiter);
+			// ASSERT
+			Assert.Equal(expected, actual);
+		}
 
-            // ASSERT
-            Assert.Equal(expected, actual);
-        }
+		[Fact]
+		public void Add_ProvidingDelimiterWithNumersAsStringInput_ReturnTheirSum()
+		{
+			// ARRANGE
+			int expected = 3;
+			string numbersWithDelimiter = "//;\n1;2";
 
-        [Fact]
-        public void Add_ProvidingNegativeNumbersAsStringInput_ExceptionShouldBeThrown()
-        {
-            // ARRANGE
-            string inputWithNegativeNumbers = "1,2,-3";
-            string expectedExceptionMessage = "-3";
+			// ACT
+			int actual = _stringCalc.Add(numbersWithDelimiter);
 
-            // ACT
-            var ex = Assert.Throws<Exception>(() => _stringCalc.Add(inputWithNegativeNumbers));
-            Assert.Equal(expectedExceptionMessage, ex.Message);
-        }
+			// ASSERT
+			Assert.Equal(expected, actual);
+		}
 
-        [Fact]
-        public void Add_AnyValidStringInput_ResultShouldBeLogged()
-        {
-            // ARRANGE
-            var validStringInput = "1,2";
-            Mock<ILogger> mockedLogger = new Mock<ILogger>();
-            var stringCalc = new StringCalculator(mockedLogger.Object);
+		[Fact]
+		public void Add_ProvidingNegativeNumbersAsStringInput_ExceptionShouldBeThrown()
+		{
+			// ARRANGE
+			string inputWithNegativeNumbers = "1,2,-3";
+			string expectedExceptionMessage = "-3";
 
-            // ACT
-            _stringCalc.Add(validStringInput);
+			// ACT
+			var ex = Assert.Throws<Exception>(() => _stringCalc.Add(inputWithNegativeNumbers));
+			Assert.Equal(expectedExceptionMessage, ex.Message);
+		}
 
-            // ASSERT
-            mockedLogger.Verify(logger => logger.Write(It.IsAny<string>()), Times.Once());
-        }
-    }
+		[Fact]
+		public void Add_AnyValidStringInput_ResultShouldBeLogged()
+		{
+			// ARRANGE
+			var validStringInput = "1,2";
+			var mockedLogger = new Mock<ILogger>();
+
+			_stringCalc = AStringCalculator().With(mockedLogger).Build();
+
+			// ACT
+			_stringCalc.Add(validStringInput);
+
+			// ASSERT
+			mockedLogger.Verify(logger => logger.Write(It.IsAny<string>()), Times.Once());
+		}
+
+		private static StringCalcBuilder AStringCalculator()
+		{
+			return new StringCalcBuilder();
+		}
+	}
+
+	internal class StringCalcBuilder
+	{
+		private Mock<ILogger> _logger = new Mock<ILogger>();
+		
+		public StringCalcBuilder With(Mock<ILogger> logger)
+		{
+			_logger = logger;
+			return this;
+		}
+
+		public StringCalculator Build()
+		{
+			return new StringCalculator(_logger.Object);
+		}
+	}
 }
